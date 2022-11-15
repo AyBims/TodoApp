0ab7c809a1d31f0d120b0f8a172408ca8a29b7b1
ayomide <adeshinaayomide12@yahoo.com>
2022-11-15 18:18:04 +0100

    add: todo and todoaccess updated, x-ray tracing implemented

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,77 @@
+import { TodoAccess } from '../dataLayer/todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+// import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid';
+// import * as createError from 'http-errors'
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { getUserId } from "../lambda/utils";
+import { getUploadUrl } from "../helpers/attachmentUtils";
+
+// TODO: Implement businessLogic
+
+const todoAccess = new TodoAccess();
+
+export async function createTodo(
+    event: APIGatewayProxyEvent,
+    newTodo: CreateTodoRequest
+  ): Promise<TodoItem> {
+    const todoId = uuid.v4();
+  
+    const userId = getUserId(event);
+    const todoItem: TodoItem = {
+      todoId,
+      userId,
+      createdAt: new Date().toISOString(),
+      name: newTodo.name,
+      dueDate: newTodo.dueDate,
+      done: false,
+      attachmentUrl: "",
+    };
+  
+    return await todoAccess.createTodo(todoItem);
+  }
+  
+  export async function getAllUserTodos(
+    event: APIGatewayProxyEvent
+  ): Promise<TodoItem[]> {
+    const userId = getUserId(event);
+    return await todoAccess.getAllUserTodos(userId);
+  }
+  
+  
+  export async function createAttachmentPresignedUrl(todoId: string): Promise<string> {
+      return getUploadUrl(todoId)
+  }
+  
+  
+  export async function updateTodo(todoId:string, event: APIGatewayProxyEvent): Promise<TodoItem> {
+    const userId = getUserId(event);
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  
+    // Check if todo created by user exists
+    const todo = await todoAccess.getTodoUsingTodoIdAndUserId(todoId, userId)
+  
+    if (!todo){
+      return null
+    }
+  
+  
+    await todoAccess.updateTodo(todoId, userId, updatedTodo)
+  }
+  
+  export async function deleteTodo(todoId: string, event: APIGatewayProxyEvent): Promise<TodoItem> {
+    const userId = getUserId(event);
+    console.log("Attempting to delete a todo", todoId, userId)
+  
+    // Check if todo created by user exists
+    const todo = await todoAccess.getTodoUsingTodoIdAndUserId(todoId, userId)
+  
+    if (!todo){
+      return null
+    }
+  
+    await todoAccess.deleteTodo(todoId, userId)
+  }
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { TodoAccess } from "../../helpers/dataLayer";
+import { TodoAccess } from "../../businessLogic/todos";
 import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
 
